e3bf30fb09689a1947918bef9da5e5dd551681f6
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-07-28 01:18:12 +0700

    Refactor QNAP build

--- build-qnap.groovy
+++ build-qnap.groovy
@@ -5,7 +5,10 @@ def f = 'get.filebot.net/qnap/index.xml' as File
 def now = new Date()
 
 
-def platforms = ['TS-NASARM', 'TS-NASX86', 'TS-X28A', 'TS-X32', 'TS-X32U', 'TS-X35A', 'TS-XA28A']
+def qnap = [
+	version: '4.3.4',
+	platforms: ['TS-NASARM', 'TS-NASX86', 'TS-X28A', 'TS-X32', 'TS-X32U', 'TS-X35A', 'TS-XA28A']
+]
 
 
 f.withWriter('UTF-8') { writer ->
@@ -23,15 +26,15 @@ f.withWriter('UTF-8') { writer ->
 			type('Utilities')
 			icon100('https://www.filebot.net/qnap/filebot_100.png')
 			icon80('https://www.filebot.net/qnap/filebot_80.png')
-			description("FileBot is the ultimate tool for organizing and renaming your Movies, TV Shows and Anime as well as fetching subtitles and artwork. It's smart and just works.")
+			description(project.properties.'filebot.package.description')
 			developer('rednoah')
 			developerLink('https://www.filebot.net/')
-			fwVersion('4.3.4')
-			version('4.8.2')
-			platforms.each{ id ->
+			fwVersion(qnap.version)
+			version(project.properties.'filebot.application.version')
+			qnap.platforms.each{ id ->
 				platform{
 					platformID(id)
-					location('https://get.filebot.net/filebot/FileBot_4.8.2/FileBot_4.8.2.qpkg')
+					location("https://get.filebot.net/filebot/FileBot_${project.properties.'filebot.application.version'}/FileBot_${project.properties.'filebot.application.version'}.qpkg")
 				}
 			}
 			snapshot('https://www.filebot.net/qnap/filebot-snapshot.png')
@@ -47,15 +50,15 @@ f.withWriter('UTF-8') { writer ->
 			type('Utilities')
 			icon100('https://www.filebot.net/qnap/filebot-node_100.png')
 			icon80('https://www.filebot.net/qnap/filebot-node_80.png')
-			description("FileBot Node allows you to execute filebot via QNAP QTS. FileBot Node requires Node.js and FileBot.")
+			description(project.properties.'filebot-node.description')
 			developer('rednoah')
 			developerLink('https://www.filebot.net/')
-			fwVersion('4.3.4')
-			version('0.2.8.1')
-			platforms.each{ id ->
+			fwVersion(qnap.version)
+			version(project.properties.'filebot-node.version')
+			qnap.platforms.each{ id ->
 				platform{
 					platformID(id)
-					location('https://get.filebot.net/qnap/packages/filebot-node_0.2.8.1.qpkg')
+					location("https://get.filebot.net/qnap/packages/filebot-node_${project.properties.'filebot-node.version'}.qpkg")
 				}
 			}
 			snapshot('https://www.filebot.net/qnap/filebot-node-snapshot.png')
@@ -64,42 +67,42 @@ f.withWriter('UTF-8') { writer ->
 
 		// Java Installer
 		item {
-			name('Java 8 Installer')
-			internalName('java-8-installer')
+			name(project.properties.'java-installer.title')
+			internalName(project.properties.'java-installer.package')
 			category('Essentials')
 			type('Developer Tools')
 			icon100('https://www.filebot.net/qnap/java-installer_100.png')
 			icon80('https://www.filebot.net/qnap/java-installer_80.png')
-			description("Java 8 Installer will help you install Oracle Java SE on your QNAP NAS. Supported platforms include armv7l, armv8, i686 and x86_64 models. During the install, this package will download the latest Oracle Java SE Development Kit (180 MB) for your platform. This may take a while.")
+			description(project.properties.'java-installer.description')
 			developer('rednoah')
 			developerLink('https://github.com/rednoah/java-installer')
-			fwVersion('4.3.4')
-			version('1.8')
-			platforms.each{ id ->
+			fwVersion(qnap.version)
+			version(project.properties.'java-installer.version')
+			qnap.platforms.each{ id ->
 				platform{
 					platformID(id)
-					location("https://get.filebot.net/qnap/packages/java-8-installer_1.8.qpkg")
+					location("https://get.filebot.net/qnap/packages/${project.properties.'java-installer.package'}_${project.properties.'java-installer.version'}.qpkg")
 				}
 			}
 		}
 
 		// Ant Installer
 		item {
-			name('Ant')
-			internalName('apache-ant')
+			name(project.properties.'ant-installer.title')
+			internalName(project.properties.'ant-installer.package')
 			category('Essentials')
 			type('Developer Tools')
 			icon100('https://www.filebot.net/qnap/apache-ant_100.png')
 			icon80('https://www.filebot.net/qnap/apache-ant_80.png')
-			description("Apache Ant Installer will help you install Apache Ant on your QNAP NAS. On install, this package will download the Apache Ant and Apache Ivy binaries and additional optional libraries. This may take a while.")
+			description(project.properties.'ant-installer.description')
 			developer('rednoah')
 			developerLink('https://github.com/rednoah/ant-installer')
-			fwVersion('4.3.4')
-			version('1.10.4')
-			platforms.each{ id ->
+			fwVersion(qnap.version)
+			version(project.properties.'ant-installer.version')
+			qnap.platforms.each{ id ->
 				platform{
 					platformID(id)
-					location("https://get.filebot.net/qnap/packages/apache-ant_1.10.4.qpkg")
+					location("https://get.filebot.net/qnap/packages/${project.properties.'ant-installer.package'}_${project.properties.'ant-installer.version'}.qpkg")
 				}
 			}
 		}
