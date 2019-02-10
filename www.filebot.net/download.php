e13e2cf1696c2d0b100017734d12de0ef1d1823f
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-02-10 13:36:00 +0700

    Redirect Linux UAs to the new general download page

--- www.filebot.net/download.php
+++ www.filebot.net/download.php
@@ -2,9 +2,16 @@
 $version = isset($_GET['version']) ? $_GET['version'] : '@{application.version}';	// default version is hard-coded via deployment script
 $type = $_GET['type'];
 
-$root = 'https://get.filebot.net/filebot/';
-$folder = $root.'FileBot_'.$version;
-$file = '';
+
+function redirect($url) {
+	error_log("[REDIRECT] ".$url);
+	header('Location: '.$url);
+	exit;
+}
+
+
+$FRS = 'https://get.filebot.net/filebot/FileBot_'.$version.'/';
+
 
 if (!isset($type)) {
 	$ua = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
@@ -13,34 +20,32 @@ if (!isset($type)) {
 	error_log("[HTTP_USER_AGENT] ".$ua);
 
 	if (preg_match('/Windows/i', $ua)) {
-		$file = 'FileBot_'.$version.'_x64.msi';
+		redirect($FRS.'FileBot_'.$version.'_x64.msi');
 	} else if (preg_match('/Macintosh/i', $ua)) {
-		$file = 'FileBot_'.$version.'.pkg';
-	} else if (preg_match('/(Debian|Ubuntu|Mint)/i', $ua)) {
-		$file = 'FileBot_'.$version.'_amd64.deb';
-	} else if (preg_match('/X11/i', $ua)) {
-		$file = 'FileBot_'.$version.'-portable.tar.xz';
+		redirect($FRS.'FileBot_'.$version.'.pkg');
+	} else {
+		redirect('https://www.filebot.net/download.html');
 	}
 } else if ($type == 'msi') {
-	$file = 'FileBot_'.$version.'_x64.msi';
+	redirect($FRS.'FileBot_'.$version.'_x64.msi');
 } else if ($type == 'zip') {
-	$file = 'FileBot_'.$version.'-portable.zip';
+	redirect($FRS.'FileBot_'.$version.'-portable.zip');
 } else if ($type == 'pkg') {
-	$file = 'FileBot_'.$version.'.pkg';
+	redirect($FRS.'FileBot_'.$version.'.pkg');
 } else if ($type == 'app') {
-	$file = 'FileBot_'.$version.'.app.tar.xz';
+	redirect($FRS.'FileBot_'.$version.'.app.tar.xz');
 } else if ($type == 'deb') {
-	$file = 'FileBot_'.$version.'_universal.deb';
+	redirect($FRS.'FileBot_'.$version.'_universal.deb');
 } else if ($type == 'portable') {
-	$file = 'FileBot_'.$version.'-portable.tar.xz';
+	redirect($FRS.'FileBot_'.$version.'-portable.tar.xz');
 } else if ($type == 'aur') {
-	$file = 'FileBot_'.$version.'-aur.tar.xz';
+	redirect($FRS.'FileBot_'.$version.'-aur.tar.xz');
 } else if ($type == 'spk') {
-	$file = 'FileBot_'.$version.'.spk';
+	redirect($FRS.'FileBot_'.$version.'.spk');
 } else if ($type == 'qpkg') {
-	$file = 'FileBot_'.$version.'.qpkg';
+	redirect($FRS.'FileBot_'.$version.'.qpkg');
+} else {
+	redirect($FRS);
 }
 
-header('HTTP/1.1 302 Found');
-header('Location: '.$folder.'/'.$file);
 ?>
