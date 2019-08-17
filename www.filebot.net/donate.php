e942fc39fd364a884d4de1d4f17eea013f80ffe7
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-08-17 16:13:24 +0700

    // redirect to purchase page

--- www.filebot.net/donate.php
+++ www.filebot.net/donate.php
@@ -1,8 +1,8 @@
 <?php
-$type = isset($_GET['src']) ? $_GET['src'] : '';
+$type = isset($_GET['src']) ? $_GET['src'] : 'undefined';
 
 // redirect to purchase page
-$url = 'https://www.filebot.net/purchase.html#'.urlencode('Upgrade '.strtoupper($type));
+$url = 'https://www.filebot.net/purchase.html#'.urlencode('upgrade-'.strtolower($type));
 
 header('HTTP/1.1 302 Found');
 header('Location: '.$url);
