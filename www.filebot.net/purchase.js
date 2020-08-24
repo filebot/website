b1d2f100265f26b46958c91f1f4fd2d190002d5d
Reinhard Pointner <reinhard.pointner@gmail.com>
2020-08-24 15:06:06 +0700

    Prefill email address to streamline license renewal

--- www.filebot.net/purchase.js
+++ www.filebot.net/purchase.js
@@ -4,13 +4,26 @@ Paddle.Setup({
 
 
 $(window).on('load', function() {
-	var deployment = location.hash.substring(1)
+	var deployment = null
+	var email = null
+
+	if (location.hash) {
+		deployment = location.hash.substring(1)
+		email = null
+	}
+
+	if (location.search) {
+		var query = new URLSearchParams(location.search)
+		deployment = query.get('deployment')
+		email = query.get('email')
+	}
 
 	if (deployment) {
 		Paddle.Checkout.open({
 			product: 532439,
 			allowQuantity: false,
-			referring_domain: deployment
+			referring_domain: deployment,
+			email: email
 		});
 	}
 });
