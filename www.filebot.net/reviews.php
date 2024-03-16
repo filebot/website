cf9f19cb835d3685ffdf14c25d13ab5197834c87
Reinhard Pointner <rednoah@filebot.net>
2024-03-16 20:38:21 +0700

    Recreate static reviews.php page since it's still in the Google index

--- www.filebot.net/reviews.php
+++ www.filebot.net/reviews.php
@@ -13,9 +13,6 @@
 
 	<link rel="preload" href="fonts/handwriting.woff2" as="font" type="font/woff2" crossorigin>
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-	<script src="reviews.js"></script>
-
 	<style>
 		div.review {
 			float: left;
@@ -32,7 +29,6 @@ fclose($handle);
 
 // parse Json
 $reviews = json_decode($contents);
-shuffle($reviews);
 
 foreach ($reviews as $id => $review) {
 ?>
