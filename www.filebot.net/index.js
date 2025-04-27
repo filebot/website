6d5bcd9b1bf65785d64a40a3ac71cd47dbb23a09
Reinhard Pointner <rednoah@filebot.net>
2025-04-27 19:31:55 +0200

    Highlight recommended download for less tech-savvy users (i.e. x64 vs arm64)

--- www.filebot.net/index.js
+++ www.filebot.net/index.js
@@ -20,4 +20,15 @@ $(window).on('load', function() {
 			a.show()
 		}
 	})
+
+	if (navigator.userAgentData) {
+		navigator.userAgentData.getHighEntropyValues(['architecture']).then(ua => {
+			if (ua.platform == 'Windows') {
+				$(ua.architecture == 'arm' ? 'a.windows-arm64' : 'a.windows-x64').addClass('highlight')
+			}
+			else if (ua.platform == 'macOS') {
+				$(ua.architecture == 'arm' ? 'a.macos-arm64' : 'a.macos-x64').addClass('highlight')
+			}
+		});
+	}
 })
